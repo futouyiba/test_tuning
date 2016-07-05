27290c4faa9edd9f5167d46ac60cfc742b43914e
futouyiba <futouyiba@gmail.com>
2016-07-05 11:20:59 +0800

    upload for the first time
    Initialize everything

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, render_to_response
+from django.http import HttpRequest, HttpResponse
+from django.template import loader
+# Create your views here.
+
+
+def index(request):
+    return render_to_response('xp/creative.html')
+
