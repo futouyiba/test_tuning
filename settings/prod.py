7a4f85e3b119f1f841f67fbf5e229e0e5e0721dd
futouyiba <futouyiba@gmail.com>
2016-07-08 22:08:16 +0800

    idea workspace是pycharm的文件

--- settings/prod.py
+++ settings/prod.py
@@ -6,3 +6,14 @@ AWS_STORAGE_BUCKET_NAME = "hyperbolagaming"
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = S3_URL
+
+DATABASES = {    
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydbname',
+        'USER' : 'futouyiba',
+        'PASSWORD' : 'bxy1367894',
+        'HOST' : 'hyperbolagaming.cfuzg7xb2jfr.us-west-2.rds.amazonaws.com',
+        'PORT' : '5432',
+    }
+}
